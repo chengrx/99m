de2ac5c3788df779bfab9788bf404345e58c97fe
Administrator <Administrator@USER-20140506SG>
2015-11-25 17:33:27 +0800

    11月25日项目从公司提交

--- App/Home/Model/UserModel.class.php
+++ App/Home/Model/UserModel.class.php
@@ -3,45 +3,51 @@ namespace Home\Model;
 use Think\Model;
 
 class UserModel extends Model{
+    /*  TP自动验证的方法
     protected $_validate = array(
         array('uname','/^[\w\d]{4,8}$/','用户名为4~8位的数字或英文！'),
         array('upwd','/^[\w\d_]{6,13}$/','密码为6~13位的数字或字母、下划线！'),
+        array('uname','require','该用户名已经存在，请换一个试试',0,'unique',1),
+        
     );
-    
+    */
     public function register($data){
-             $info = array();
-          //  $pattern_user = "/^[a-zA-Z-_]{4,20}$/";
-            // $pattern_pwd = "/^[a-zA-Z0-9]{6,13}$/";
-/*
-            if(count($this->where("uname='{$data['username']}'")->select())>0){
+           $info = array();
+           $pattern_user = "/^[a-zA-Z-_]{4,20}$/";
+           $pattern_pwd = "/^[a-zA-Z0-9]{6,13}$/";
+
+            if(count($this->where("uname='{$data['uname']}'")->select())>0){
                  $info = array(
                 'status' => 0,
                 'info' => "您注册的用户名已经存在，请换一个试试",
             );
                  return $info;
             }
-            if($data['pwd']==''){
+            if($data['upwd']==''){
                 $info = array(
                 'status' => 0,
                 'info' => '密码不得为空',
             );
                 return $info;
            }    
-            if($data['pwd']!=$data['pwd_confirm']){
+
+ 
+            if($data['upwd']!=$data['pwd_confirm']){
                $info = array(
                 'status' => 0,
                 'info' => '密码输入不一致，请重新输入',
             );
               return $info;
            }
-           if(!preg_match($pattern_pwd,$data['pwd'])){
+           
+           if(!preg_match($pattern_pwd,$data['upwd'])){
                 $info = array(
                    'status' => 0,
                    'info' => '密码格式不正确',
                 );
                 return $info;  
             }
-             if(!preg_match($pattern_user,$data['username'])){
+             if(!preg_match($pattern_user,$data['uname'])){
                  $info = array(
                 'status' => 0,
                 'info' => '用户名格式不正确',
@@ -50,14 +56,15 @@ class UserModel extends Model{
             }
  
 
-            $data['pwd'] = md5($data['pwd']); //加密
-*/ 
+            $data['upwd'] = md5($data['upwd']); //加密
+
             //---数据入库--------------------------
            $data_arr['uname'] = $data['uname'];
            $data_arr['upwd'] = $data['upwd'];
            $data_arr['ip'] = $_SERVER['REMOTE_ADDR'];
            $data_arr['last_time'] = time();
-           
+       
+           /*  TP自动验证的方法
             //$User = D("User"); // 实例化User对象
             if (!$this->create($data_arr,1)){ // 指定新增数据
                  // 如果创建失败 表示验证没有通过 输出错误提示信息
@@ -80,8 +87,8 @@ class UserModel extends Model{
            
             }
     }
-           
-/*
+       */    
+
             if(($this->add($data_arr))>0){ //var_dump(M('User')->add($data_arr)) 默认返回的是记录的主键值，因为主键值都不为0，只需>0,则添加记录成功
                //return '恭喜注册成功';              // M('User') 换成$this->  效率更高，
                 $info = array(
@@ -97,7 +104,7 @@ class UserModel extends Model{
                 return $info;
             }
     }
-    */
+   
     
     //--登录验证--------------------------------------
     public function login($username,$pwd){
--- App/Runtime/Cache/Home/08afd3323aea03c4d961611b7d1e6c34.php
+++ App/Runtime/Cache/Home/08afd3323aea03c4d961611b7d1e6c34.php
@@ -5,6 +5,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width">
         <script src="/99m/Public/static/jquery-1.11.3.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="/99m/Public/css/user.css"/>
     </head>
     <body>
         <!DOCTYPE html>
@@ -23,6 +24,57 @@
     </div>
 </body>
 </html>
+        
+          <div class="login_wrap">
+           <div class="login_wrapL">
+               <div class="slide_box">
+                   <div class="bd">
+                       <ul style="position:relative;width: 468px;height:0px;">
+                           <li style="position:absolute;width: 468px;left: 0px;top:0px;display: none;">
+                               <a href="" target="_blank"><img src="/99m/Public/img/user/login_01.gif"/></a>
+                            </li>
+                           <li style="position:absolute;width: 468px;left:0px;top: 0px;display: list-tiem;">
+                            <img src="/99m/Public/img/user/login_02.gif"/></li>
+                           </li>
+                       </ul>
+                    </div>
+                   <div class="hd">
+                       <ul>
+                           <li class="on"></li>
+                           <li class="2"></li>
+                       </ul>
+                   </div>
+               </div>
+            </div>
+            <div class="login_wrapR"> 
+                  <!--右侧-->
+                  <span>如有账号，请登录</span>
+                  <div class="register_box">
+                   <form class="register" id="register_form" action="<?php echo U('Home/User/register');?>" method="post">
+                       <p id="account_tip" class="error"></p>  
+                       <p class="user_text">
+                           用户名：&nbsp;&nbsp;&nbsp;<input type="text" name="username" id="username" class="username" placeholder="邮箱/用户名/手机号"/>
+                           <i class="userico"></i>
+                       </p>
+                       <p id="account_tip" class="error"></p> 
+                       <p class="user_text">
+                           密 &nbsp;码：&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="password" name="pwd" id="pwd" class="userpwd" placeholder="密码"/>
+                           <i class="userico"></i>
+                     </p>
+                      <p class="user_text">
+                           确认密码：<input type="password" name="confirm_pwd" id="pwd_confirm" class="userpwd" placeholder="确认密码"/>
+                           <i class="userico"></i>
+                     </p>
+                     <input type="button" value="注 册" id="to_register" style="width:80px;">
+                     &nbsp;&nbsp;<a href='login.html'>登 录</a>
+                 </form>
+                      <p id="register_info" style="color:red;"></p>
+                  </div>
+             </div> 
+          
+       </div>
+
+        <!--
         <p id="notice"></p>
         <form id="register_form" action="<?php echo U('Home/User/register');?>" method="post">
                 <p>用户名:&nbsp;<input type="text" id="username" name="username"/></p>
@@ -31,14 +83,16 @@
                 <input type="button" id="btn" value="提交注册"/>
                 <a href='login.html'>登录</a>
             </form>
+        -->
         <script>
             $(function(){
-                $('#btn').click(function(){
+                $('#to_register').click(function(){
                     var $uname = $('#username').val();
                     console.log($uname);
                     var $upwd = $('#pwd').val();
                     console.log($upwd);
                     var $pwd_confirm = $('#pwd_confirm').val();
+                    console.log($pwd_confirm);
                     var $action = $('#register_form').attr('action');
                     $.post($action,{uname:$uname, upwd:$upwd, pwd_confirm:$pwd_confirm},function(data){
                         console.log(data);
