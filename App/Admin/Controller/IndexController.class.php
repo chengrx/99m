45ce9a30a0b46366c198906ab1819467dd2404d7
Administrator <Administrator@USER-20140506SG>
2015-12-08 17:37:30 +0800

    12月8日项目从公司提交

--- App/Admin/Controller/IndexController.class.php
+++ App/Admin/Controller/IndexController.class.php
@@ -3,6 +3,6 @@ namespace Admin\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $this->show('Hello admin!');
+        $this->display();
     }
 }
---
+++ App/Runtime/Cache/Admin/9df924d9a2d217daa77a359c5bee0c33.php
@@ -0,0 +1,25 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+    <head>
+        <title>后台管理系统</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" type="text/css" src='/99m/Public/static/bootstrap/css/bootstrap.css'/>
+        <link rel="stylesheet" type='text/css' src='/99m/Public/css/admin.css'/>
+    </head>
+    <body>
+    
+        <div class="container-fluid header">
+            <div class='row'>
+        顶部通栏
+            </div>
+        </div>
+    
+    左侧菜单栏
+    右侧内容区
+    
+        <div class='container-fluid'>
+        底部版权信息
+         </div>
+    </body>
+</html>
